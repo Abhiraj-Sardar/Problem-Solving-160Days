e9572929b66a64f779f14156ed9a6a03c06ef3b1
ABHIRAJ SARDAR <abhirajsardar2003@gmail.com>
2024-12-05 21:36:31 +0530

    updated Day3

---
+++ Arrays-1/Easy/ReverseanArray.py
@@ -0,0 +1,37 @@
+# Reverse an Array
+
+# You are given an array of integers arr[]. Your task is to reverse the given array.
+
+# Examples:
+
+# Input: arr = [1, 4, 3, 2, 6, 5]
+# Output: [5, 6, 2, 3, 4, 1]
+# Explanation: The elements of the array are 1 4 3 2 6 5. After reversing the array, the first element goes to the last position, the second element goes to the second last position and so on. Hence, the answer is 5 6 2 3 4 1.
+# Input: arr = [4, 5, 2]
+# Output: [2, 5, 4]
+# Explanation: The elements of the array are 4 5 2. The reversed array will be 2 5 4.
+# Input: arr = [1]
+# Output: [1]
+
+# Explanation: The array has only single element, hence the reversed array is same as the original.
+
+# Constraints:
+# 1<=arr.size()<=105
+# 0<=arr[i]<=105
+
+class Solution:
+    def reverseArray(self, arr):
+        # code here
+        n = len(arr)
+        start=0
+        end=n-1
+        
+        while start<end:
+            arr[start],arr[end]=arr[end],arr[start]
+            start+=1
+            end-=1
+            
+        return arr
+        
+        # TC=O(N)
+        # SC=O(1)
